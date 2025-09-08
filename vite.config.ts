7adb56b31f4a9706eee817497ba22eebe574f78b
William <Williamlv1998@gmail.com>
2025-09-08 07:34:39 -0500

    midifciando vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/cicd/', // ⚡ muy importante
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
